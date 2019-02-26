da5401a54e42686c403841b920fc4419626feaa8
Deepak VP <vpdeepak@gmail.com>
2019-02-26 16:48:28 +0100

    Homework #5: Basic Loops

---
+++ BasicLoops/main.py
@@ -0,0 +1,30 @@
+"""
+This is the solution for the Homework #5: Basic Loops
+
+"""
+
+print("Assignment on Basic Loops")
+
+for num in range(1, 101, 1) :
+    show = num
+    if(num % 3 == 0 and num % 5 == 0) :
+        show = "FizzBuzz"
+    elif(num % 3 == 0) :
+        show = "Fizz"
+    elif(num % 5 == 0) :
+        show = "Buzz"
+
+    counter = 0
+    for i in range(1, num + 1, 1) :
+        if(num % i == 0) :
+            counter += 1
+            if(counter > 2) :
+                break
+
+    if(counter == 2) :
+        show = "prime"
+         
+
+    print(show)
+
+
