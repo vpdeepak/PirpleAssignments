39219dc9b814534e376a909b2d5975c93febfb5a
Deepak VP <vpdeepak@gmail.com>
2019-02-27 13:33:02 +0100

    Homework #6: Advanced Loops

---
+++ AdvancedLoops/main.py
@@ -0,0 +1,38 @@
+"""
+This is the solution for the Homework #6: Advanced Loops
+
+"""
+
+print("Assignment on Advanced Loops")
+
+def DrawBoard(rows, columns) :
+    result = False
+    if(rows < 70 and columns < 235) :
+        result = True
+        for row in range(rows) :
+            if(row % 2 == 0) :
+                for column in range(columns) :
+                    if(column % 2 == 0) :
+                        if(column != columns - 1) :
+                            print(" ", end = "")
+                        else:
+                            print(" ")
+                    else :
+                        if(column != columns - 1) :
+                            print("|", end = "")
+                        else:
+                            print("|")
+            else :
+                for column in range(columns) :
+                    if(column != columns - 1) :
+                        print("-", end = "")
+                    else :
+                        print("-")
+    return result
+
+result = DrawBoard(69, 234)
+
+if(result == False) :
+    print("Playing Board doesn't fit the screen !! Please re-consider rows and column values")
+
+
